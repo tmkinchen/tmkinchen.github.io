5035ac4bd3b8f2fbeedc1674ac14a548b7b4abb0
tmkinchen <97756277+tmkinchen@users.noreply.github.com>
2022-04-11 13:56:45 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -12,8 +12,8 @@ var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
 L.geoJSON(data, {
   style: function(feature){
-  var alertColor = 'orange';
-  if (feature.properties.severity === 'Severe') alertColor = 'Red';
+  var alertColor = 'Coral';
+  if (feature.properties.severity === 'Severe') alertColor = 'Crimson';
   if (feature.properties.severity === 'Moderate') alertColor = 'Yellow';
   return { color: alertColor };
 },
