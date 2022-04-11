437c57e564456d902066e13dff0fd79722178f2f
tmkinchen <97756277+tmkinchen@users.noreply.github.com>
2022-04-11 13:26:48 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -13,7 +13,7 @@ $.getJSON(weatherAlertsUrl, function(data) {
 L.geoJSON(data, {
   style: function(feature){
   var alertColor = 'orange';
-  if (feature.properties.severity === 'Severe') alertColor = 'red';
+  if (feature.properties.severity === 'Minor') alertColor = 'Purple';
   return { color: alertColor };
 },
 onEachFeature: function(feature, layer) {
